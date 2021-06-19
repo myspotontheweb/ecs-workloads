c242b9ac3c2d606eeaecb81c000b3a0a0406daf6
Mark O'Connor <mark@myspotontheweb.com>
2021-06-20 00:30:07 +0100

    Add remote backend configuration

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,13 @@
+terraform {
+  backend "remote" {
+    organization = "myspotontheweb"
+
+    workspaces {
+      name = "ecs-workloads-1"
+    }
+  }
+}
+
 provider "aws" {
   region = "eu-west-1"
 }
